3c13d2f589768c84dcaec5c30a3eacbc148f8b52
whatznottechnology <whatznottechnology@gmail.com>
2025-07-24 02:48:19 +0530

    Remove django_browser_reload from production URLs

--- Source/urls.py
+++ Source/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('theme.urls')),  # Frontend theme URLs
     path('projects/', include('Projects.urls')),
     path('land-leads/', include('land_leads.urls')),
